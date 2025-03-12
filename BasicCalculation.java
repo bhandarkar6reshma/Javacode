6caf85ea1b7652367e696f011b450080a91dab75
bhandarkar6reshma <bhandarkar6reshma@gmail.com>
2025-03-12 11:01:51 +0530

    Add files via upload

---
+++ BasicCalculation.java
@@ -0,0 +1,32 @@
+class BasicCalculation {
+public static void main(String[] args) {
+        double a=45;
+	double b=20;
+	double c=a+b;
+	double d=a-b;
+	double e=a*b;
+	double f=a/b;
+	double g=a%b;
+        System.out.println("Simple Calculation:");
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println(f);
+	System.out.println(g);
+    }
+}
+
+Output
+
+D:\Software_Testing\src\New folder>javac BasicCalculation.java
+
+D:\Software_Testing\src\New folder>java BasicCalculation
+Simple Calculation:
+65.0
+25.0
+900.0
+2.25
+5.0
+
+D:\Software_Testing\src\New folder>
+
